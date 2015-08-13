abab78f6d50e2dc8cba173fc685e8389d8904060
Kuriakose Sony Theakanath <Sony@kuriakosesmbp2.attlocal.net>
2015-08-12 19:57:25 -0700

    inital commit

---
+++ Split/Split/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Split
+//
+//  Created by Kuriakose Sony Theakanath on 8/12/15.
+//  Copyright (c) 2015 Kuriakose Sony Theakanath. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Split/Split/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Split
+//
+//  Created by Kuriakose Sony Theakanath on 8/12/15.
+//  Copyright (c) 2015 Kuriakose Sony Theakanath. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
